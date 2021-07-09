e89c436fc6130780c3337cf0bfc5d08f43c5687a
David Forero <davidforero717@gmail.com>
2021-07-09 01:59:46 -0500

    se realiza autenticacion con angular 2

--- frontend-konectar/src/app/login/login.component.ts
+++ frontend-konectar/src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { UsuarioService } from '../services/usuario.service';
 import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -11,39 +12,53 @@ import { ToastrService } from 'ngx-toastr';
 export class LoginComponent implements OnInit {
   form: FormGroup;
   submitted = false;
-  data:any;
+  data: any;
+  token: any;
 
-  constructor(private formBuilder:FormBuilder, private usuarioService:UsuarioService, private toastrService:ToastrService) { }
+  constructor(private formBuilder: FormBuilder, private usuarioService: UsuarioService, private toastrService: ToastrService, private router: Router) { }
 
-  LoginForm(){
+  LoginForm() {
 
     this.form = this.formBuilder.group({
-      email:['',[Validators.required,Validators.email]],
-      password:['',[Validators.required,Validators.minLength(6)]]
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
     });
 
   }
-  
+
   ngOnInit(): void {
     this.LoginForm();
   }
 
-  get f(){
+  get f() {
     return this.form.controls;
   }
 
-  submit(){
+  submit() {
     this.submitted = true;
-    if(this.form.invalid){
+    if (this.form.invalid) {
       return;
     }
-    this.usuarioService.login(this.form.value).subscribe(res=>{
-    this.data = res;
-    console.log(res);
-    
-    if(this.data.status ===1){
+    this.usuarioService.login(this.form.value).subscribe(res => {
+      this.data = res;
+      console.log(res);
 
-    }
+      if (this.data.status === 1) {
+        this.token = this.data.data.token;
+        localStorage.setItem('token',this.token);
+        this.router.navigate(['/']);
+        this.toastrService.success(JSON.stringify(this.data.message),JSON.stringify(this.data.code),{
+          timeOut:2000,
+          progressBar:true
+        });
+      }else if(this.data.status === 0){
+        this.toastrService.error(JSON.stringify(this.data.message),JSON.stringify(this.data.code),{
+          timeOut:2000,
+          progressBar:true
+        });
+      }
+
+
     });
   }
 
